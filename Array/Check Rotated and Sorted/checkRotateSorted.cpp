3802d9625a018ec03235ed32b0b539bd3fbeadf7
ashwinrmcf <ashwinrmcf@gmail.com>
2025-09-02 16:43:13 +0530

    update

--- Array/Check Rotated and Sorted/checkRotateSorted.cpp
+++ Array/Check Rotated and Sorted/checkRotateSorted.cpp
@@ -1,3 +1,21 @@
-//
-// Created by ashwi on 02/09/2025.
-//
+#include<bits/stdc++.h>
+using namespace std;
+bool checkRotateSorted(vector<int>& arr){
+    int n=arr.size();
+    int count=0;
+    for(int i=0;i<n;i++){
+      if(arr[i]>arr[(i+1)%n]){
+        count++;
+      }
+    }
+    return (count<=1);
+}
+int main(){
+  vector<int> arr={5,6,7,3,5};
+  if(checkRotateSorted(arr)){
+    cout<<"Yes, its sorted and rotated"<<endl;
+  }else{
+    cout<<"No, its not sorted or rotated"<<endl;
+  }
+  return 0;
+}
